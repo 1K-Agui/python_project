3f31f914c01d680976a795a93465421e142b3f33
1K-Agui <kiprotichk225@gmail.com>
2023-11-05 11:52:09 +0300

    initial commit

---
+++ prime numbers.py
@@ -0,0 +1,28 @@
+def addition (num1, num2):
+    results = num1 + num2
+    print("{0}+{1}={2}".format(num1,num2,results))
+
+def subtraction (num1, num2):
+    results = num1 - num2
+    print("{0}-{1}={3}".format(num1,num2,results))
+
+def multiplication (num1, num2):
+    results = num1 * num2
+    print("{0}*{1}={2}".format(num1,num2,results))
+
+def division (num1, num2):
+    if num2 ==0.0:
+        print("INVALID CHOICE")
+    else:
+        results = num1 / num2
+        print("{0}/{1}={2}".format(num1,num2,results))
+
+
+
+
+
+
+
+
+
+
